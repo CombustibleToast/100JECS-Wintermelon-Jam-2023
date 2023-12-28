c38854320753586d7707900080033d8be915f297
CombustibleToast <robert.sobert24@gmail.com>
2023-12-27 22:03:58 -0500

    Changed inputs from UI actions to custom mapped WASD.

--- Scripts/main character.gd
+++ Scripts/main character.gd
@@ -17,18 +17,15 @@ func _physics_process(delta):
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 	#	velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	# Don't know what that means yet so ui actions for now -Ena
 	# Handle X input
-	var x_direction = Input.get_axis("ui_left", "ui_right")
+	var x_direction = Input.get_axis("left", "right")
 	if x_direction:
 		velocity.x = x_direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED) #lerp?
 	
 	# Handle Y input
-	var y_direction = Input.get_axis("ui_up", "ui_down")
+	var y_direction = Input.get_axis("up", "down")
 	if y_direction:
 		velocity.y = y_direction * SPEED
 	else:
