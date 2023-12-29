b44feebdabe244b5886061eacbd2bb7657cdea59
CombustibleToast <robert.sobert24@gmail.com>
2023-12-29 17:02:26 -0500

    Inventory slots can now be assigned items

---
+++ Scripts/charm slot gui.gd
@@ -0,0 +1,22 @@
+extends Panel
+
+@onready var backgroundSprite: Sprite2D = $"slot background"
+@onready var charmSprite: Sprite2D = $"charm"
+
+func update(item: CharmInventoryItem):
+	if(!item):
+		charmSprite.visible = false
+		#Todo: Also disable clicking interaction
+	else:
+		charmSprite.visible = true
+		charmSprite.texture = item.texture
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- Scripts/inventory view.gd
+++ Scripts/inventory view.gd
@@ -2,6 +2,17 @@ extends Control
 
 var is_open: bool = false
 
+# load inventory model
+@onready var inventory: CharmInventory = preload("res://Resources/charm inventory.tres")
+@onready var slots: Array = $GridContainer.get_children()
+
+func _ready():
+	update()
+
+func update():
+	for i in range(min(inventory.items.size(), slots.size())):
+		slots[i].update(inventory.items[i])
+
 func open():
 	visible = true
 	is_open = true
@@ -11,3 +22,4 @@ func close():
 	is_open = false
 
 # Made using this tutorial https://www.youtube.com/watch?v=qLrj_HWA9CI
+
