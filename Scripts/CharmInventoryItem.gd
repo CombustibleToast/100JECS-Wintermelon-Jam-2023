7e268615174ffcac631935193cc2d0d7fa875b60
CombustibleToast <robert.sobert24@gmail.com>
2023-12-29 15:12:19 -0500

    Started making actual inventory

---
+++ Scripts/CharmInventoryItem.gd
@@ -0,0 +1,5 @@
+extends Resource
+
+class_name CharmInventoryItem
+
+@export var texture: Texture2D
---
+++ Scripts/InventoryModel.gd
@@ -0,0 +1,5 @@
+extends Resource
+
+class_name CharmInventory
+
+@export var items: Array[CharmInventoryItem]
