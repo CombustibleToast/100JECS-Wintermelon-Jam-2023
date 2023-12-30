61c46844b58d87bc80b5075cdbdd2627079656ea
countchrisdo <crburwell@yahoo.com>
2023-12-29 21:17:39 -0500

    Dialogue Depth expanded

---
+++ Scripts/state.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var apple_status: String = ""
+
+var attraction: int = 0
+var affability: int = 3
+var flattery: int = 2
+var mystery: int = 3
+var confidence: int = 1
+
+var loveGodot: bool = false
