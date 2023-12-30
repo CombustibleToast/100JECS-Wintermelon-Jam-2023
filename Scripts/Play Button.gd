b0aad537dc8fc2a4b5e37e525247decc52249cca
CombustibleToast <robert.sobert24@gmail.com>
2023-12-30 17:05:18 -0500

    Updated updated title scene

--- Scripts/Play Button.gd
+++ Scripts/Play Button.gd
@@ -3,7 +3,6 @@ extends Button
 func _ready():
 	print(name + " ready")
 
-# Function stub created by Godot and linked to this function.
-func _on_pressed():
-	print("pressed 2!")
+func _pressed():
+	print("pressed 1!")
 	get_tree().change_scene_to_file("res://scenes/main scene.tscn")
